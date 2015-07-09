01a0e341d63d823aaa353845a28be6b658ca60a8
Cameron Stewart <bacchus@unm.edu>
2015-07-09 16:16:16 -0600

    fixed silly formatting errors left by emacs

--- Ereserves Organize/migrateEreserves.py
+++ Ereserves Organize/migrateEreserves.py
@@ -168,15 +168,15 @@ class Migration:
 				# Open or create 'info' file that relates filename to title (if title exists)
 				info_file_path = destination_path + "/" + "info.txt"
 
-                                if self.run_mode == 'RUN':
-                                        info_file = open(info_file_path, 'a+')
+				if self.run_mode == 'RUN':
+					info_file = open(info_file_path, 'a+')
 
 				# Have to assume that there is only one file in origin.
 				for f in glob.glob(origin_path+"/*"):
 					try:
 						filename, file_extension = os.path.splitext(f)
 						logging.info("Copying file: %s to %s" % (f, destination_path))
-                                                filename_no_path = f.rsplit('/', 1)[1]
+						filename_no_path = f.rsplit('/', 1)[1]
 						logging.info("Info File info: Filename: %s\nTitle(Description): %s\n\n" % (filename_no_path, title))
 						if self.run_mode == 'RUN':
 							shutil.copy(f, destination_path)
@@ -184,9 +184,9 @@ class Migration:
 								info_file.write("Filename: %s\nTitle(Description): %s\n\n" % (filename_no_path, title))
 					except shutil.Error as err:
 						logging.warning(err)
-                                
-                                if self.run_mode == 'RUN':
-                                        info_file.close()
+
+				if self.run_mode == 'RUN':
+					info_file.close()
 
 			row = cursor.fetchone()
 		

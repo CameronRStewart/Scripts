5b5bc9662427c2ba626ddff081537df28c1bcf7b
Cameron Stewart <bacchus@unm.edu>
2015-12-07 11:31:28 -0700

    switches to working solution for sherlock

--- Brainteasers/PHP/sherlock-and-beast.php
+++ Brainteasers/PHP/sherlock-and-beast.php
@@ -17,22 +17,59 @@ for ($i = 0; $i < $test_cases_count; $i++){
 	$digits[] = fgets($_fp);
 }
 
-for ($i = 0; $i < $test_cases_count; $i++){
-	if ($digits[$i] < 3){
+foreach ($digits as $digit){
+	if($digit < 3) {
 		print "-1\n";
+		continue;
 	}
-	else {
-		// Maybe do for 5 and then 3 to see if there is a proper number of digits.
+	list($div_by_3, $mod_by_3) = divmod($digit, 3);
+	$count = $div_by_3;
+	//print " div_by_3: " . $div_by_3 . " mod_by_3: " . $mod_by_3 . "\n";
+	for($i = 0; $i <= $count; $i++){
 
-		// divisible by 5
-		list($div_by_5, $mod_by_5) = divmod((int)$digits[$i], 5);
-		// divisible by 3
-		list($div_by_3, $mod_by_3) = divmod((int)$digits[$i], 3);
-		print ("Divisible by 5 " . $div_by_5 . " mod " . $mod_by_5 . "\n");
-		print ("Divisible by 3 " . $div_by_3 . " mod " . $mod_by_3 . "\n");
+		$new_div_by_3 = $div_by_3 - $i;
+		$new_mod_by_3 = $mod_by_3 + (3 * $i);
+		//print " div_by_3: " . $new_div_by_3 . " mod_by_3: " . $new_mod_by_3 . "\n";
+
+
+		if($new_mod_by_3 == 0){
+			$threes = 0;
+			$fives = $new_div_by_3 * 3;
+			print_decent_numbers($threes, $fives);
+			break;
+		}
+		else{
+			if($new_mod_by_3 >= 5) {
+				list($div_by_5, $mod_by_5) = divmod($new_mod_by_3, 5);
+				//print "mod_by_3: " . $new_mod_by_3 . " div_by_5: " . $div_by_5 . " mod_by_5: " . $mod_by_5 . "\n";
+				if ($mod_by_5 == 0){
+					$threes = $div_by_5 * 5;
+					$fives = $new_div_by_3 * 3;
+					print_decent_numbers($threes, $fives);
+					break;
+				}
+			}
+			if ($i == $count) {
+				print "-1\n";
+				break;
+			}
+		}
 	}
 }
 
+function print_decent_numbers($threes, $fives) {
+	if($threes == 0 && $fives == 0){
+		print "-1\n";
+		return;
+	}
+	for ($i = 0; $i < $fives; $i++){
+		print '5';
+	}
+	for ($i = 0; $i < $threes; $i++){
+		print '3';
+	}
+	print "\n";
+}
 
 function divmod($number, $divisor){
 	$div = floor($number / $divisor);
